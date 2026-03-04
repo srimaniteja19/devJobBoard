a68074fbbd1cb7ed260ec87fca5b4a0f021d62c8
Maniteja Chinnam <sreemanitejateja@gmail.com>
2026-03-03 19:21:43 -0500

    added extension feature

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      {
+        module: /node_modules\/unpdf\/dist\/pdfjs\.mjs/,
+        message: /Critical dependency: the request of a dependency is an expression/,
+      },
+    ];
+    return config;
+  },
+};
 
 module.exports = nextConfig;
